0465ecf0e526d26bc64e8b48bbbd318dd096b9af
hieu313 <hieunm3103@gmail.com>
2022-09-21 21:34:00 +0700

    nodejs

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express')
+const morgan = require('morgan')
+const { engine } = require('express-handlebars');
+const app = express()
+const port = 3000
+//! HTTP logger
+app.use(morgan('combined'))
+//! Template Engine:
+app.engine('hbs', engine({extname: '.hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, './resource/views'));
+//! Method
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
